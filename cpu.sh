2e09e372cbb7b677356e41cd2875683467110bf5
Hwang,Jupil <namoo4u@users.noreply.github.com>
2018-04-23 23:44:36 +0900

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "\"cpu_threads_conf\" : [ " > cpu.txt
 cpu_cnt=$(lscpu | grep Core | awk '{print $4}')
-for ((n=0;n<$cpu_cnt;n++)); do echo "{ \"low_power_mode\" : false, \"no_prefetch\" : true, \"affine_to_cpu\" : $n }," >> cpu.txt ; done
+socket_cnt=$(lscpu | grep Socket | grep -v grep | awk '{print $2}')
+for ((n=0;n<$cpu_cnt*$socket_cnt - 1;n++)); do echo "{ \"low_power_mode\" : false, \"no_prefetch\" : true, \"affine_to_cpu\" : $n }," >> cpu.txt ; done
 echo "]," >> cpu.txt
